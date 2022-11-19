bdb43c0f7792f5bbd52ab9df30e627a9c6960a8b
Nicolas <vazqueznicolasdev@gmail.com>
2022-11-19 20:23:44 -0300

    Landing Home styles

--- components/LandingHome/LandingHome.tsx
+++ components/LandingHome/LandingHome.tsx
@@ -1,11 +1,12 @@
 import styles from "./landingHome.module.css";
+import { FiExternalLink } from "react-icons/fi";
 
 export const LandingHome = () => {
 	return (
 		<section className={styles.container}>
 			<div className={styles.landingBackground}></div>
 			<div className={styles.landingInfo}>
-				<div>
+				<div className={styles.landingInfo__text}>
 					<h1>Wear the best.</h1>
 					<p className="color-grey">
 						Still, the second option holds promise could make the tagline shortlist if you
@@ -14,8 +15,18 @@ export const LandingHome = () => {
 				</div>
 
 				<div className={styles.landingInfo__buttons}>
-					<button>Shop now</button>
-					<button>See promo</button>
+					<button>
+						Shop now
+						<span>
+							<FiExternalLink />
+						</span>
+					</button>
+					<button>
+						See promo
+						<span>
+							<FiExternalLink />
+						</span>
+					</button>
 				</div>
 			</div>
 		</section>

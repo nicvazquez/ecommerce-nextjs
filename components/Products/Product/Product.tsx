f1c748869101c177b247ef45d573cbc3a32337b0
Nicolas <vazqueznicolasdev@gmail.com>
2022-11-19 17:57:16 -0300

    Desktop styles

--- components/Products/Product/Product.tsx
+++ components/Products/Product/Product.tsx
@@ -1,6 +1,6 @@
 import { Rating } from "react-simple-star-rating";
-import Image from "next/image";
 import { Product as ProductInterface } from "../../../interfaces";
+import Image from "next/image";
 import styles from "./product.module.css";
 
 export const Product = ({ product }: { product: ProductInterface }) => {
@@ -9,26 +9,28 @@ export const Product = ({ product }: { product: ProductInterface }) => {
 			<Image
 				src={product.image}
 				alt={`${product.title} Product`}
-				width={300}
-				height={300}
+				width={200}
+				height={200}
 			/>
 
-			<p className="color-grey">{product.category}</p>
-			<div className={styles.titlePrice}>
-				<h3>{product.title}</h3>
-				<p className={styles.price}>${product.price}</p>
+			<div>
+				<p className="color-grey">{product.category}</p>
+				<div className={styles.titlePrice}>
+					<h3>{product.title}</h3>
+					<p className={styles.price}>${product.price}</p>
+				</div>
+
+				<p className="color-grey">{product.description.split(".")[0]}.</p>
+				<Rating
+					style={{ marginTop: "1rem" }}
+					initialValue={product.rating.rate}
+					size={20}
+					readonly
+					allowFraction
+				/>
+
+				<button className={styles.addToCarBtn}>Add to cart</button>
 			</div>
-
-			<p className="color-grey">{product.description.split(".")[0]}.</p>
-			<Rating
-				style={{ marginTop: "1rem" }}
-				initialValue={product.rating.rate}
-				size={20}
-				readonly
-				allowFraction
-			/>
-
-			<button className={styles.addToCarBtn}>Add to cart</button>
 		</div>
 	);
 };

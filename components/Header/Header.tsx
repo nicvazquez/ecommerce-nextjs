72e6faf25ecc6332e97f0ef545dcbc5123ddb16a
Nicolas <vazqueznicolasdev@gmail.com>
2022-11-20 16:35:07 -0300

    Navbar

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,49 @@
+import styles from "./header.module.css";
+import { useEffect, useState } from "react";
+import { HiMenuAlt2 } from "react-icons/hi";
+import { FaTimes } from "react-icons/fa";
+import Link from "next/link";
+
 export const Header = () => {
-	return <div>Header</div>;
+	const [showMenu, setShowMenu] = useState(false);
+
+	const links = [
+		{
+			name: "Home",
+			link: "/",
+		},
+		{
+			name: "Products",
+			link: "/products",
+		},
+		{
+			name: "Promo",
+			link: "/",
+		},
+		{
+			name: "About",
+			link: "/",
+		},
+	];
+
+	return (
+		<header className={styles.header}>
+			<nav>
+				<p>WearTheBest</p>
+
+				<button onClick={() => setShowMenu(!showMenu)}>
+					{showMenu ? <FaTimes /> : <HiMenuAlt2 />}
+				</button>
+				{showMenu && (
+					<div className={styles.links}>
+						{links.map((link) => (
+							<div key={link.name}>
+								<Link href={link.link}>{link.name}</Link>
+							</div>
+						))}
+					</div>
+				)}
+			</nav>
+		</header>
+	);
 };

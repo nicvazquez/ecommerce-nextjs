20a97aa1d0127f64b7a6e6e240a46a01347e917a
Nicolas <vazqueznicolasdev@gmail.com>
2022-11-20 15:16:32 -0300

    Related products added

--- components/Products/Product/Product.tsx
+++ components/Products/Product/Product.tsx
@@ -11,38 +11,38 @@ interface ProductProps {
 export const Product = ({ product, fullDescription = false }: ProductProps) => {
 	return (
 		<div className={styles.container} key={product.id}>
-			<Image
-				src={product.image}
-				alt={`${product.title} Product`}
-				width={200}
-				height={200}
-			/>
+			<Link href={`/products/${product.id}`}>
+				<Image
+					src={product.image}
+					alt={`${product.title} Product`}
+					width={200}
+					height={200}
+				/>
 
-			<div>
-				<p className="color-grey">{product.category}</p>
-				<div className={styles.titlePrice}>
-					<h3>
-						<Link href={`/products/${product.id}`}>{product.title}</Link>
-					</h3>
-					<p className={styles.price}>${product.price}</p>
-				</div>
+				<div>
+					<p className="color-grey">{product.category}</p>
+					<div className={styles.titlePrice}>
+						<h3>{product.title}</h3>
+						<p className={styles.price}>${product.price}</p>
+					</div>
 
-				<p className="color-grey">
-					{fullDescription ? (
-						product.description
-					) : (
-						<span>{product.description.slice(0, 150)}...</span>
-					)}
-				</p>
-
-				<div className={styles.rating}>
 					<p className="color-grey">
-						<span>{product.rating.rate} ⭐</span> ({product.rating.count} reviews)
+						{fullDescription ? (
+							product.description
+						) : (
+							<span>{product.description.slice(0, 150)}...</span>
+						)}
 					</p>
-				</div>
 
-				<button className={`btn-primary ${styles.addToCarBtn}`}>Add to cart</button>
-			</div>
+					<div className={styles.rating}>
+						<p className="color-grey">
+							<span>{product.rating.rate} ⭐</span> ({product.rating.count} reviews)
+						</p>
+					</div>
+
+					<button className={`btn-primary ${styles.addToCarBtn}`}>Add to cart</button>
+				</div>
+			</Link>
 		</div>
 	);
 };
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,22 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
 import { Product } from "../../components/Products/Product/Product";
 import { ProductInterface } from "../../interfaces";
+import styles from "./singleProduct.module.css";
 
-export default function ProductPage({
-	product,
-}: {
+interface Props {
 	product: ProductInterface;
-}) {
+	relatedProducts: ProductInterface[];
+}
+
+export default function ProductPage({ product, relatedProducts }: Props) {
+	relatedProducts.map((product) => {
+		console.log(product.title);
+	});
 	return (
-		<div
-			style={{
-				height: "100vh",
-				width: "100%",
-				display: "grid",
-				placeItems: "center",
-				padding: "2rem",
-			}}
-		>
-			<Product product={product} fullDescription />
+		<div className={styles.container}>
+			<>
+				<Product product={product} fullDescription />
+
+				<div className={styles.relatedProductsContainer}>
+					<h3>Related Products</h3>
+
+					<div className={styles.relatedProducts}>
+						{relatedProducts.map((prod) => (
+							<div className={styles.relatedProducts__card} key={prod.id}>
+								<Link href={`/products/${prod.id}`}>
+									<Image src={prod.image} alt={`${prod.title} Image`} width={100} height={100} />
+									<h4>{prod.title.slice(0, 60)}...</h4>
+									<p>${prod.price}</p>
+								</Link>
+							</div>
+						))}
+					</div>
+				</div>
+			</>
 		</div>
 	);
 }
@@ -38,12 +55,25 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }: { params: ProductInterface }) {
-	const res = await fetch(`https://fakestoreapi.com/products/${params.id}`);
-	const product = await res.json();
+	const resProduct = await fetch(
+		`https://fakestoreapi.com/products/${params.id}`
+	);
+	const product = await resProduct.json();
+
+	const resProducts = await fetch(`https://fakestoreapi.com/products/`);
+	const products = await resProducts.json();
+
+	const relatedProducts = products
+		.filter(
+			(prod: ProductInterface) =>
+				prod.category === product.category && prod.id !== product.id
+		)
+		.slice(0, 5);
 
 	return {
 		props: {
 			product,
+			relatedProducts,
 		},
 	};
 }
